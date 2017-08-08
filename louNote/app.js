4f1ae68770cd7924d52df6d776bf9fe73dcf7f67
WaterWan <598127577@qq.com>
2017-08-08 20:34:27 +0800

    ..

---
+++ louNote/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+
+// 生成一个 express 实例
+var app = express();
+
+// 设置视图文件存放目录
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 设置静态文件存放目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 解析 urlencoded 请求体必备
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: '首页'
+    });
+});
+
+app.listen(3000, function(req, res) {
+    console.log('app is running at port 3000');
+});
